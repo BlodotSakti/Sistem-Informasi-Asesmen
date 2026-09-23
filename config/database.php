287acf5a0876a8486232d2cc6c3236803afa66d8
BlodotSakti <blodot.122140045@student.itera.ac.id>
2026-09-23 08:21:00 +0700

    fix: hapus hardcode path mysqldump laragon agar support di server linux railway

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
                 Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'dump' => [
-                'dump_binary_path' => env('DUMP_BINARY_PATH', 'C:/laragon/bin/mysql/mysql-8.4.3-winx64/bin/'),
+                'dump_binary_path' => env('DUMP_BINARY_PATH', ''),
                 'use_single_transaction',
                 'timeout' => 60 * 5,
             ],
